3ef5c4aa28817f152f49a241af9c9c937b7a5695
devgabrielchagas <gabriel.chagas2044@gmail.com>
2025-03-26 16:34:43 -0300

    update: model Car has 2 new fields, plate and photo. add: installed package Pillow. update: requirements.txt update with new packages

--- cars/admin.py
+++ cars/admin.py
@@ -8,7 +8,7 @@ class BrandAdmin(admin.ModelAdmin):
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model','brand','factory_year','model_year','value')
-    search_fields = ('model', 'brand')
+    search_fields = ('model',)
 
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
