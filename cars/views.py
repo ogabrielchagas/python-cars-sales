fab81ea6f0297eab3961874793661827b315e31b
devgabrielchagas <gabriel.chagas2044@gmail.com>
2025-03-27 14:17:56 -0300

    add: Django ORM to map our DB records (cars) to Objects and then rendering them into our template cars.html using Django templates language

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all()
+    print(cars)
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'} }
+        {'cars': cars}
     )
