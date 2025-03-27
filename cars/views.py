514ef762a3da9e673999fd646aa2093cd269ae74
devgabrielchagas <gabriel.chagas2044@gmail.com>
2025-03-27 15:54:19 -0300

    update: cars.html with new css and html improvements to better present the frontend, also was included a form to work as a search field to find specifics cars filtered by model of the car

--- cars/views.py
+++ cars/views.py
@@ -5,9 +5,9 @@ def cars_view(request):
     filter = request.GET.get('search')
 
     if filter:
-        cars = Car.objects.filter(model__icontains=filter)
+        cars = Car.objects.filter(model__icontains=filter).order_by('model')
     else:
-        cars = Car.objects.all()
+        cars = Car.objects.all().order_by('model')
 
     return render(
         request, 
