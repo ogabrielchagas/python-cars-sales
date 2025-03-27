6057e799e676d14668b9e4a7f5e220a85a254594
devgabrielchagas <gabriel.chagas2044@gmail.com>
2025-03-27 12:38:24 -0300

    add: new route cars/ added to urls.py and add: a new view and simple html Template to this new endpoint cars/

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cars_view(request):
+    html = '''
+        <html>
+            <head>
+                <title>Meus Carros</title>
+            </head>
+            <body>
+                <h1>Carros da Chagas Revendas</h1>
+                <h3>Só carro top!</h3>
+            </body>
+        </html>
+    '''
+
+    return HttpResponse(html)
