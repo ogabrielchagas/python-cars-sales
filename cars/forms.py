eeb04c08d5646058c17b7647392423891eec3258
devgabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-07 16:48:05 -0300

    update: in cars.html changed car.value to car.value|floatformat:2. update: in forms.py added functions clean_value and clean_factory_year to add some validation to this two fields in our form.

--- cars/forms.py
+++ cars/forms.py
@@ -6,3 +6,16 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do veículo deve ser de R$20.000,00')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Ano de fabricação mínimo do veículo deve ser 1975')
+        return factory_year
+    
