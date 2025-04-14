40cdfcdda486e4e4f5480247ff77586ef1c94161
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-14 20:00:44 -0300

    update: changed car_view function to utilize Class Based Views (CBVs) instead. Now we render our class CarsView which executes the same code from car_view in its get function

--- cars/views.py
+++ cars/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    filter = request.GET.get('search')
+class CarsView(View):
+    
+    def get(self, request):
+        filter = request.GET.get('search')
 
-    if filter:
-        cars = Car.objects.filter(model__icontains=filter).order_by('model')
-    else:
-        cars = Car.objects.all().order_by('model')
+        if filter:
+            cars = Car.objects.filter(model__icontains=filter).order_by('model')
+        else:
+            cars = Car.objects.all().order_by('model')
 
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars}
-    )
+        return render(
+            request, 
+            'cars.html', 
+            {'cars': cars}
+        )
+
+
 
 def new_car_view(request):
     if request.method == 'POST':
@@ -26,6 +31,7 @@ def new_car_view(request):
         new_car_form = CarModelForm()
     
     return render(request, 'new_car.html', { 'new_car_form': new_car_form }) 
+
 
 
     
