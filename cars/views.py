8f5137c92b7772fd6781a14fe220047c1fe43c4b
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-14 21:21:00 -0300

    update: changed NewCarView to a new type of view NewCarCreateView which is a generic view from Django that is used specifically to views which we going to have a form and capture data from the client side

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
     
 class CarsListView(ListView):
     model = Car
@@ -18,18 +18,11 @@ class CarsListView(ListView):
 
         return cars
     
-class NewCarView(View):
-    
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form }) 
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
 
     
 
