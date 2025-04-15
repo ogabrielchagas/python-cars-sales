e815350edde29e1899ea3a5c6722634bd5a80207
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-14 21:03:17 -0300

    update: changed CarsView to a new type of view CarsListView which utilizes the ListViews from CBVs from Django framework

--- cars/views.py
+++ cars/views.py
@@ -2,22 +2,21 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-
-class CarsView(View):
+from django.views.generic import ListView
     
-    def get(self, request):
-        filter = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-        if filter:
-            cars = Car.objects.filter(model__icontains=filter).order_by('model')
-        else:
-            cars = Car.objects.all().order_by('model')
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model') 
+        #get.queryset() por padrao retornar object.all()
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
 
-        return render(
-            request, 
-            'cars.html', 
-            {'cars': cars}
-        )
+        return cars
     
 class NewCarView(View):
     
@@ -30,7 +29,7 @@ class NewCarView(View):
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+        return render(request, 'new_car.html', { 'new_car_form': new_car_form }) 
 
     
 
