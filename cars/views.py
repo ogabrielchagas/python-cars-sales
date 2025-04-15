13ae33bc94b2e6c205678e70e184aa226e602e11
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-15 14:05:01 -0300

    update: added method decorators in views.py in cars app in 3 views: NewCarCreateView, CarUpdateView and CarDeleteView to require a authenticated user to access these endpoints, if it doesnt have a authenticated user we redirect to the login endpoint

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
     
 class CarsListView(ListView):
@@ -19,6 +19,7 @@ class CarsListView(ListView):
 
         return cars
     
+@method_decorator(login_required(login_url='login'), name='dispatch')    
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -29,6 +30,7 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')   
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -36,7 +38,8 @@ class CarUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
-    
+
+@method_decorator(login_required(login_url='login'), name='dispatch')       
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
