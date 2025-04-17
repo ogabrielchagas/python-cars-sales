8ef7c81b67853da9e21f115fe3f38833fa1e7d02
ogabrielchagas <gabriel.chagas2044@gmail.com>
2025-04-16 22:27:55 -0300

    update: signals.py, created a new function car_inventory_update that has the algorhitm to calculate the total amount of cars from inventory and their total value and updates the CarIventory with the new updated Inventory

--- cars/signals.py
+++ cars/signals.py
@@ -1,16 +1,23 @@
 from django.db.models.signals import post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
+def car_inventory_update():
+    cars_count = Car.objects.all().count()  #Total of cars in inventory
+    cars_total_value = Car.objects.aggregate(
+        total_value = Sum('value')      #{'total_value': 100.00} returns from query set
+    )['total_value']                    #acessing total_value field
+
+    CarInventory.objects.create(        #create a new entry into CarIventory table/model
+        cars_count = cars_count,        #With the total of cars calculated before
+        cars_total_value = cars_total_value #The total value of those cars
+    )
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    cars_count = Car.objects.all().count()
-    cars_total_value = Car.objects.aggregate(
-        total_value = Sum('value')
-    )
+    car_inventory_update()
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    cars_count = Car.objects.all().count()
+    car_inventory_update()
